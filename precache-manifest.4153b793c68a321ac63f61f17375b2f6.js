f2ec6e2313de72e8e26066e4e08117c431fdfb33
unknown <vijayraj8266@gmail.com>
2020-09-16 23:14:33 +0530

    Updates

---
+++ precache-manifest.4153b793c68a321ac63f61f17375b2f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e019a3ee58638ee9de25d92caff0df2",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c5e840ebc78db743b0ee",
+    "url": "/monsters-rolodex/static/css/main.741a1fce.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5e840ebc78db743b0ee",
+    "url": "/monsters-rolodex/static/js/main.15789794.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
